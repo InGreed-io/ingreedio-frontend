f3c26e9014684e18a7531c8d50b6416cf222280e
Konrad Adamiec <61701333+adamieck@users.noreply.github.com>
2024-03-01 16:48:55 +0100

    add routing and directory structure (#4)

---
+++ src/screens/Landing.jsx
@@ -0,0 +1,14 @@
+import { InputGroup, Input, InputLeftElement } from "@chakra-ui/input";
+export const Landing = () => {
+  return (
+    <>
+      <p>Landing!!</p>
+      <InputGroup>
+        <InputLeftElement>
+          Test
+        </InputLeftElement>
+        <Input type='text' placeholder='' />
+      </InputGroup>
+    </>
+  )
+}
