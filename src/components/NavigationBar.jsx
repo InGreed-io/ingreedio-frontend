aae41770b04e699b0d3fd1e467609286eb7df409
adamieck <KAdamiec196@gmail.com>
2024-03-09 20:14:05 +0100

    nav bar lint

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,40 +1,40 @@
 import {
-    Box, Image, Button, Center, ButtonGroup,
-    Grid
+  Box, Image, Button, Center, ButtonGroup,
+  Grid
 } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 
 export const NavigationBar = () => {
-    return (
-        <Grid templateColumns='repeat(3, 1fr)' gap={6}>
-            <Box alignSelf="center" justifySelf="start">
-                <Link to="/">
-                    <Image src="/logo.svg" />
-                </Link>
-            </Box>
-            <Center>
-                <ButtonGroup gap="5" variant="link">
-                    <Button>
+  return (
+    <Grid templateColumns='repeat(3, 1fr)' gap={6}>
+      <Box alignSelf="center" justifySelf="start">
+        <Link to="/">
+          <Image src="/logo.svg" />
+        </Link>
+      </Box>
+      <Center>
+        <ButtonGroup gap="5" variant="link">
+          <Button>
                         About Us
-                    </Button>
-                    <Button>
+          </Button>
+          <Button>
                         Pricing
-                    </Button>
-                    <Button>
+          </Button>
+          <Button>
                         Terms and Conditions
-                    </Button>
-                </ButtonGroup>
-            </Center>
-            <Box alignSelf="center" justifySelf="end">
-                <ButtonGroup gap="5">
-                    <Button variant="link">
+          </Button>
+        </ButtonGroup>
+      </Center>
+      <Box alignSelf="center" justifySelf="end">
+        <ButtonGroup gap="5">
+          <Button variant="link">
                         Log In
-                    </Button>
-                    <Button>
+          </Button>
+          <Button>
                         Sign Up
-                    </Button>
-                </ButtonGroup>
-            </Box>
-        </Grid>
-    )
-}
+          </Button>
+        </ButtonGroup>
+      </Box>
+    </Grid>
+  );
+};
